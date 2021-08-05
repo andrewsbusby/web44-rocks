288f280581476f7c2560cc443065ac9cd30676d3
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-05 11:10:27 -0600

    wip

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('web44 rocks');
+require('dotenv').config(); // this allows to 'inject' fake enviroment varialbes
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.use('*', (req, res, next) => {
+    res.json({
+        message: "web44 is awesome indeed"
+    })
+})
+
+server.listen(process.env.PORT || 5000, () => { // heroku machine uses process.env.PORT
+    console.log(`server running ${process.env.PORT || 5000}`)
+})
